b81594cb3b5c12cd0599abe45dee33c57bead0ce
66hakeem <66hakeem@gmail.com>
2018-12-07 00:11:11 +0300

    Modify create red flag

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Users:
     def __init__(self):
         self.users = []
     
-    def register_user(self, firstname, lastname, email, phonenumber, username, othername = ''):
+    def register_user(self, firstname, lastname, email, phonenumber, username, othername):
         """ functions adds a user to the users dictionary """
         user_id = len(self.users) + 1
         isAdmin = False
@@ -30,3 +30,33 @@ class Users:
     def get_users(self):
         """ gets a list of users"""
         return jsonify({'users':self.users}),200
+
+class Records:
+    def __init__(self):
+        self.records = []
+
+    def create_red_flag_record(self, createdBy, location, comment, images=list()):
+        record_id = len(self.records) + 1
+        status = "Draft"
+        created_on = str(date.today())
+        record_type = "red-flag"
+
+        images = []
+
+        record = {
+            "id": record_id,
+            "createdOn": created_on,
+            "createdBy": createdBy,
+            "type": record_type,
+            "location": location,
+            "status": status,
+            "comment": comment,
+            "images": images
+        }
+
+        self.records.append(record)
+        return jsonify({'message':'successfully added'}), 201
+    
+    def get_red_flags(self):
+        """ gets a list of users"""
+        return jsonify({'records':self.records}),200
--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import jsonify, request
-from app.models import Users
+from app.models import Users, Records
 from instance import myapp
 
 user1 = Users()
+record1 = Records()
 
 @myapp.route('/api/v1/users', methods=['POST'])
 def register_user():
@@ -17,3 +18,17 @@ def register_user():
 @myapp.route('/api/v1/users', methods=['GET'])
 def get_users():
     return user1.get_users()
+
+@myapp.route('/api/v1/records', methods=['POST'])
+def create_red_flag():
+    createdBy = request.json['createdBy']
+    location = request.json['location']
+    comment = request.json['comment']
+    images_list = []
+    images_list.append(request.json['images'])
+   
+    return record1.create_red_flag_record(createdBy, location, comment, images_list)
+
+@myapp.route('/api/v1/records', methods=['GET'])
+def get_red_flag_records():
+    return record1.get_red_flags()
