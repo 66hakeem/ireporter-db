e9be58aa3ccdd7cb07b66e3bd3f919696773a5e6
66hakeem <66hakeem@gmail.com>
2018-12-10 17:10:17 +0300

    Add tests

--- test/test_api.py
+++ test/test_api.py
@@ -19,3 +19,17 @@ class ApiTest(TestCase):
         response = self.app.get('/api/v1/users')
         self.assertEqual(response.status_code, 200)        
     
+    def test_create_redflag(self):
+        add_redflag = self.app.post('/api/v1/records', content_type='application/json',
+        data=json.dumps(dict(createdBy="2", location="23.444,44,3", comment="Fallen Electric Pole", 
+        images="[bridge.jpg, water.png]", videos="[video1.mp4]" )))
+
+        self.assertEqual(add_redflag.status_code, 201)
+    
+    def test_get_all_redflag_records(self):
+        recordsresponse = self.app.get('/api/v1/records')
+        self.assertEqual(recordsresponse.status_code, 200)  
+
+    def test_get_specific_redflag(self):
+        flag = self.app.get('api/v1/records/1')
+        self.assertEqual(flag.status_code, 200) 
