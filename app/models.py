56337074c66db4f4e30a5f854e216d62cc9fa710
66hakeem <66hakeem@gmail.com>
2019-01-22 13:34:29 +0300

    Add create user db enpoint to verify db queries work

--- app/models.py
+++ app/models.py
@@ -1,102 +1,49 @@
 from flask import jsonify
+from app.db import Database
 from datetime import date
+import psycopg2
+from werkzeug.security import generate_password_hash, check_password_hash
 
+
+db_content = Database()
 
 class Users:
 
-    def __init__(self):
-        self.users = []
-
     def register_user(self, firstname, lastname, email, phonenumber, username,
-                      othername):
-        """ functions adds a user to the users dictionary """
-        user_id = len(self.users) + 1
+                      othername, password ):
+        
         isAdmin = False
-        today = str(date.today())
-
-        if not firstname.isalpha() or len(firstname) < 2 or " " in firstname:
-            return jsonify({"message": "wrong first name format"}), 400
-
-        if not lastname.isalpha() or len(lastname) < 2 or " " in lastname:
-            return jsonify({"message": "wrong last name format"}), 400
-
-        if not phonenumber.isdigit() or len(phonenumber) < 7 or " " in \
-                phonenumber:
-            return jsonify({"message": "wrong phone number format"}), 400
-
-        if not othername.isalpha() or len(othername) < 2 or " " in othername:
-            return jsonify({"message": "wrong other name format"}), 400
-
-        if " " in email:
-            return jsonify({"message": "Email must not have spaces"}), 400
-
-        if len(username) < 5 or " " in username:
-            return jsonify({"message": "wrong user name format"}), 400
-
-        for user in self.users:
-            if user['username'] == username:
-                return jsonify({"message": "Username already exists"}), 409
-
-        user = {
-            "firstname": firstname,
-            "user_id": user_id,
-            "lastname": lastname,
-            "email": email,
-            "phonenumber": phonenumber,
-            "username": username,
-            "othername": othername,
-            "isAdmin": isAdmin,
-            "registered": today
-        }
-        self.users.append(user)
-        return jsonify({"status": 201, "data": [{"id": user_id, "message":
-                        "Created user."}]}), 201
+        registered = str(date.today())
+        try:
+            sql="INSERT INTO users(firstname,lastname,email,phonenumber,username,othername,password,registered,isAdmin) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            db_content.cur.execute(sql,(firstname,lastname,email,phonenumber,username,othername,password,registered,isAdmin))
+        except psycopg2.Error as err:
+            return jsonify({'error':str(err)})        
+        return jsonify({'message':'succussfully registered'}),201
+    
 
     def get_users(self):
-        """ gets a list of users"""
-        return jsonify({"status": 200, "data": self.users}), 200
-
-
+        db_content.dict_cursor.execute("SELECT * FROM users")
+        data=db_content.dict_cursor.fetchall() 
+        return jsonify({'results':data})
+"""
 class Records:
-    def __init__(self):
-        self.records = []
-
+    
     def create_red_flag_record(self, createdBy, location, comment, images,
-                               videos):
-        record_id = len(self.records) + 1
-        status = "Draft"
-        created_on = str(date.today())
-        record_type = "red-flag"
+                               videos, ):
 
-        record = {
-            "id": record_id,
-            "createdOn": created_on,
-            "createdBy": createdBy,
-            "type": record_type,
-            "location": location,
-            "status": status,
-            "comment": comment,
-            "images": images,
-            "videos": videos
-        }
-
-        self.records.append(record)
-        return jsonify({"status": 201, "data": [{"id": record_id, "message":
-                        "Created red-flag record"}]}), 201
-
+        
+        
     def get_red_flags(self):
-        """ gets a list of users"""
         return jsonify({"status": 200, "data": self.records}), 200
 
     def get_red_flag(self, record_id):
-        """get a specific red flag"""
         for record in self.records:
             if record['id'] == record_id:
                 return jsonify({"status": 200, "data": record}), 200
             return jsonify({"message": "Record does not exist"})
 
     def delete_red_flag(self, record_id):
-        """deletes a red flag record"""
         for record in self.records:
             if record['id'] == record_id:
                 self.records.remove(record)
@@ -105,7 +52,6 @@ class Records:
                                 }), 200
 
     def edit_redflag_location(self, record_id, location):
-        """Edit a red-flag's location"""
         for record in self.records:
             if record['id'] == record_id:
                 record['location'] = location
@@ -114,7 +60,6 @@ class Records:
                                                          }]}), 200
 
     def edit_redflag_comment(self, record_id, comment):
-        """Edit a red-flag's location"""
         for record in self.records:
             if record['id'] == record_id:
                 record['comment'] = comment
@@ -123,7 +68,6 @@ class Records:
                                                         ]}), 200
 
     def edit_redflag(self, record_id, location, comment, images, videos):
-        """Edit a red-flag"""
         for record in self.records:
             if record['id'] == record_id:
                 record['location'] = location
@@ -133,3 +77,4 @@ class Records:
 
                 return jsonify({"status": 200, "message":
                                 "Updated red-flag"}), 200
+"""
--- app/views.py
+++ app/views.py
@@ -1,76 +1,28 @@
 from flask import jsonify, request
-from app.models import Users, Records
+from app.models import Users
 from instance import myapp
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 user1 = Users()
-record1 = Records()
 
 
 @myapp.route('/api/v1/users', methods=['POST'])
 def register_user():
-    firstname = request.json['firstname']
-    lastname = request.json['lastname']
-    email = request.json['email']
-    phonenumber = request.json['phonenumber']
-    username = request.json['username']
-    othername = request.json['othername']
-    return user1.register_user(firstname, lastname, email, phonenumber,
-                               username, othername)
-
+    
+    try:
+        firstname = request.json['firstname']
+        lastname = request.json['lastname']
+        email = request.json['email']
+        password = generate_password_hash(request.json['password'], method='sha256')
+        phonenumber = request.json['phonenumber']
+        username = request.json['username']
+        othername = request.json['othername']
+    except KeyError:
+         return jsonify({'message':'some fields are missing'}),400   
+    return user1.register_user(firstname, lastname, email, phonenumber, username, othername, password)
 
 @myapp.route('/api/v1/users', methods=['GET'])
 def get_users():
+
     return user1.get_users()
-
-
-@myapp.route('/api/v1/red_flags', methods=['POST'])
-def create_red_flag():
-    createdBy = request.json['createdBy']
-    location = request.json['location']
-    comment = request.json['comment']
-    images = []
-    images.append(request.json['images'])
-    videos = []
-    videos.append(request.json['videos'])
-
-    return record1.create_red_flag_record(createdBy, location, comment, images,
-                                          videos)
-
-
-@myapp.route('/api/v1/red_flags', methods=['GET'])
-def get_red_flag_records():
-    return record1.get_red_flags()
-
-
-@myapp.route('/api/v1/red_flags/<int:red_flag_id>', methods=['GET'])
-def get_specific_redflag(red_flag_id):
-    return record1.get_red_flag(red_flag_id)
-
-
-@myapp.route('/api/v1/red_flags/<int:red_flag_id>', methods=['DELETE'])
-def delete_redflag_records(red_flag_id):
-    return record1.delete_red_flag(red_flag_id)
-
-
-@myapp.route('/api/v1/red_flags/<int:red_flag_id>/location', methods=['PATCH'])
-def edit_location(red_flag_id):
-    location = request.json['location']
-    return record1.edit_redflag_location(red_flag_id, location)
-
-
-@myapp.route('/api/v1/red_flags/<int:red_flag_id>/comment', methods=['PATCH'])
-def edit_comment(red_flag_id):
-    comment = request.json['comment']
-    return record1.edit_redflag_comment(red_flag_id, comment)
-
-
-@myapp.route('/api/v1/red_flags/<int:red_flag_id>', methods=['PUT'])
-def edit_red_flag(red_flag_id):
-    location = request.json['location']
-    comment = request.json['comment']
-    images = []
-    images.append(request.json['images'])
-    videos = []
-    videos.append(request.json['videos'])
-
-    return record1.edit_redflag(red_flag_id, location, comment, images, videos)
