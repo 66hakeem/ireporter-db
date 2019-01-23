f6d1576cb7c52f9747ecb0c99d78934ac6330843
66hakeem <66hakeem@gmail.com>
2019-01-23 16:20:34 +0300

    Add endpoint for updating comment in redflag

--- app/controllers.py
+++ app/controllers.py
@@ -17,13 +17,13 @@ class Users:
             sql = "INSERT INTO users(firstname,lastname,email,phonenumber,username,othername,password,registered,isAdmin) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
             db_content.cur.execute(sql, (firstname, lastname, email, phonenumber, username, othername, password, registered, isAdmin))
         except psycopg2.Error as err:
-            return jsonify({'error': str(err)})        
+            return jsonify({'error': str(err)})
         return jsonify({'message': 'succussfully registered'}), 201
 
     def get_users(self):
         db_content.dict_cursor.execute("SELECT * FROM users")
         data = db_content.dict_cursor.fetchall()
-        return jsonify({'results': data})
+        return jsonify({'users': data})
 
 
 class Incidents:
@@ -37,5 +37,20 @@ class Incidents:
             sql = "INSERT INTO incidents(createdOn,createdBy,incident_type,location,status,comment,images,videos) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
             db_content.cur.execute(sql, (createdOn, createdBy, incident_type, location, status, comment, images, videos))
         except psycopg2.Error as err:
-            return jsonify({'error': str(err)})        
+            return jsonify({'error': str(err)})
         return jsonify({'message': 'succussfully registered'}), 201
+
+    def get_all_redflags(self):
+        db_content.dict_cursor.execute("SELECT * FROM incidents")
+        data = db_content.dict_cursor.fetchall()
+        return jsonify({'red-flags': data})
+
+    def update_redflag_comment(self, id, comment):
+        sql = "UPDATE incidents SET comment='"+comment+"' WHERE id='{}'".format(id)
+        db_content.cur.execute(sql)
+        return jsonify({'message': 'Comment successfully updated'}), 200
+    
+    def update_redflag_location(self, id, location):
+        sql = "UPDATE incidents SET comment='"+location+"' WHERE id='{}'".format(id)
+        db_content.cur.execute(sql)
+        return jsonify({'message': 'Location successfully updated'}), 200
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import jsonify, request, make_response
-from app.models import Users, Incidents
+from app.controllers import Users, Incidents
 from instance import myapp
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.db import Database
-from flask_jwt_extended import (JWTManager, verify_jwt_in_request, jwt_required, create_access_token, get_jwt_identity)
+from flask_jwt_extended import (JWTManager, create_access_token, get_jwt_identity, jwt_required)
 import datetime
 from functools import wraps
 
@@ -11,6 +11,25 @@ record = Incidents()
 user1 = Users()
 db_cont = Database()
 jwt = JWTManager(myapp)
+
+@myapp.route('/login', methods=['GET'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify')
+
+    sql = "SELECT username, password, user_id from users WHERE username = %s"
+    db_cont.dict_cursor.execute(sql, (auth.username,))
+    user = db_cont.dict_cursor.fetchone()
+    if not user:
+        return make_response('User not found')
+    if check_password_hash((user['password']), auth.password):
+        token = create_access_token(identity=auth.username)
+        return jsonify({'token': token})
+    return make_response('Could not verify')
+
+
 
 
 @myapp.route('/api/v1/users', methods=['POST'])
@@ -30,28 +49,10 @@ def register_user():
 
 
 @myapp.route('/api/v1/users', methods=['GET'])
-@jwt_required
 def get_users():
-    
+
     return user1.get_users()
 
-
-@myapp.route('/login', methods=['GET'])
-def login():
-    auth = request.authorization
-
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify')
-
-    sql = "SELECT username, password, user_id from users WHERE username = %s"
-    db_cont.dict_cursor.execute(sql, (auth.username,))
-    user = db_cont.dict_cursor.fetchone()
-    if not user:
-        return make_response('User not found')
-    if check_password_hash((user['password']), auth.password):
-        token = create_access_token(identity=auth.username)
-        return jsonify({'token': token})
-    return make_response('Could not verify')
 
 @myapp.route('/api/v1/red_flags', methods=['POST'])
 @jwt_required
@@ -61,7 +62,24 @@ def create_red_flag():
         location = request.json['location']
         comment = request.json['comment']
         images = request.json['images']
-        videos = request.json['videos']    
+        videos = request.json['videos']
     except KeyError:
-        return jsonify({'message': 'some fields are missing'}), 400 
+        return jsonify({'message': 'some fields are missing'}), 400
     return record.create_red_flag_record(createdBy, location, comment, images, videos)
+
+
+@myapp.route('/api/v1/red_flags', methods=['GET'])
+@jwt_required
+def get_red_flags():
+
+    return record.get_all_redflags()
+
+
+@myapp.route('/api/v1/red_flags/<int:red_flag_id>/comment', methods=['PATCH'])
+def update_comment(red_flag_id):
+    try:
+        comment = request.json['comment']
+    except KeyError:
+        return jsonify({'message': 'Comment field is missing'}), 400
+
+    return record.update_redflag_comment(red_flag_id, comment)
