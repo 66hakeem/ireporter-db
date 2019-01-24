b5b6e9107eb11e0bb44cc530ef6f18ddec5f2e7b
66hakeem <66hakeem@gmail.com>
2019-01-24 16:32:06 +0300

    Modify views.py

--- app/views.py
+++ app/views.py
@@ -6,7 +6,8 @@ from instance import myapp
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.db import Database
 from flask_jwt_extended import (JWTManager, create_access_token,
-                                get_jwt_identity, jwt_required)
+                                get_jwt_identity, verify_jwt_in_request,
+                                jwt_required)
 import datetime
 from functools import wraps
 
@@ -14,14 +15,16 @@ db_cont = Database()
 jwt = JWTManager(myapp)
 
 
-@myapp.route('/login', methods=['GET'])
+@myapp.route('/api/v1/auth/login', methods=['GET'])
 def login():
+    """Login to your account"""
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify')
 
-    sql = "SELECT username, password, user_id from users WHERE username = %s"
+    sql = "SELECT username, password, user_id, isAdmin from users WHERE\
+             username = %s"
     db_cont.dict_cursor.execute(sql, (auth.username,))
     user = db_cont.dict_cursor.fetchone()
     if not user:
@@ -31,10 +34,26 @@ def login():
         return jsonify({'token': token})
     return make_response('Could not verify')
 
+"""
+def admin(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        auth = request.authorization
+        userr = get_jwt_identity()
+        sql = "SELECT username, isAdmin from users WHERE username = %s"
+        db_cont.dict_cursor.execute(sql, (auth.username,))
+        usere = db_cont.dict_cursor.fetchone()
+        if usere['isAdmin'] == 'False':
+            return jsonify({'msg': 'Admins only!'}), 403
+        else:
+            return f(*args, **kwargs)
+    return wrapper
+"""
+
 
 @myapp.route('/api/v1/users', methods=['POST'])
 def register_user():
-
+    """Create user"""
     try:
         firstname = request.json['firstname']
         lastname = request.json['lastname']
@@ -51,14 +70,16 @@ def register_user():
 
 
 @myapp.route('/api/v1/users', methods=['GET'])
+@jwt_required
 def get_users():
-
+    """Get all users"""
     return User().get_users()
 
 
 @myapp.route('/api/v1/red_flags', methods=['POST'])
 @jwt_required
 def create_red_flag():
+    """Create a red-flag"""
     try:
         createdBy = get_jwt_identity()
         location = request.json['location']
@@ -72,13 +93,15 @@ def create_red_flag():
 
 
 @myapp.route('/api/v1/red_flags', methods=['GET'])
+@jwt_required
 def get_red_flags():
-
+    """Get all red-flags"""
     return Redflag().get_all_redflags()
 
 
 @myapp.route('/api/v1/red_flags/<int:red_flag_id>/comment', methods=['PATCH'])
 def update_red_flag_comment(red_flag_id):
+    """Update red-flag comment"""
     try:
         comment = request.json['comment']
     except KeyError:
@@ -89,36 +112,106 @@ def update_red_flag_comment(red_flag_id):
 
 @myapp.route('/api/v1/red_flags/<int:red_flag_id>/location', methods=['PATCH'])
 def update_location(red_flag_id):
+    """Update red-flag location"""
     try:
         location = request.json['location']
     except KeyError:
         return jsonify({'message': 'Location field is missing'}), 400
 
-    return Redflag().update_redflag_location(red_flag_id, location)
+    update = Redflag().update_redflag_location(red_flag_id, location)
+    
+    return jsonify({"status": 200, "data": [{"message": "Updated red-flag\
+                         record's location"}]}), 200
 
 
 @myapp.route('/api/v1/red_flags/<int:red_flag_id>', methods=['GET'])
 def get_specific_redflag(red_flag_id):
+    """Get a specific red-flag"""
     return Redflag.get_red_flag(red_flag_id)
 
 
 @myapp.route('/api/v1/red_flags/<int:red_flag_id>', methods=['DELETE'])
 def delete_redflag_record(red_flag_id):
+    """Delete a specific red-flag"""
     return Redflag().delete_redflag(red_flag_id)
+
+
+@myapp.route('/api/v1/intervention', methods=['POST'])
+@jwt_required
+def create_intervention():
+    """Create an intervention record"""
+    try:
+        createdBy = get_jwt_identity()
+        location = request.json['location']
+        comment = request.json['comment']
+        images = request.json['images']
+        videos = request.json['videos']
+    except KeyError:
+        return jsonify({'message': 'some fields are missing'}), 400
+    return Intervention().create_intervention_record(createdBy, location, 
+                                                     comment, images, videos)
 
 
 @myapp.route('/api/v1/interventions', methods=['GET'])
 def get_interventions():
-
+    """Get all intervention records"""
     return Intervention().get_all_interventions()
+
+
+@myapp.route('/api/v1/intervention/<int:intervention_id>', methods=['GET'])
+def get_specific_intervention(intervention_id):
+    """Get a specific intervention"""
+    return Intervention.get_intervention(intervention_id)
 
 
 @myapp.route('/api/v1/intervention/<int:intervention_id>/comment',
              methods=['PATCH'])
 def update_intervention_comment(intervention_id):
+    """Update an intervention record's comment"""
     try:
         comment = request.json['comment']
     except KeyError:
         return jsonify({'message': 'Comment field is missing'}), 400
 
     return Intervention().update_intervention_comment(intervention_id, comment)
+
+
+@myapp.route('/api/v1/intervention/<int:intervention_id>/location',
+             methods=['PATCH'])
+def update_intervention_location(intervention_id):
+    """Update intervention location"""
+    try:
+        comment = request.json['comment']
+    except KeyError:
+        return jsonify({'message': 'Comment field is missing'}), 400
+
+    return Intervention().update_intervention_location(intervention_id,
+                                                       location)
+
+
+@myapp.route('/api/v1/intervention/<int:intervention_id>', methods=['DELETE'])
+def delete_intervention_record(intervention_id):
+    """Delete a specific red-flag"""
+    return Intervention().delete_intervention(intervention_id)
+
+
+@myapp.route('/api/v1/intervention/<int:intervention_id>/status',
+             methods=['PATCH'])
+@jwt_required
+def update_intervention_status(intervention_id):
+    """Update an intervention record's status"""
+    try:
+        user_logged = get_jwt_identity()
+        sql = "SELECT username, isAdmin from users WHERE username = %s"
+        db_cont.dict_cursor.execute(sql, (user_logged,))
+        user = db_cont.dict_cursor.fetchone()
+        if user['isAdmin'] == 'False':
+            return jsonify({'msg': 'Admins only!'}), 403
+        else:
+            status = request.json['status']
+            return Intervention().update_intervention_status(intervention_id,
+                                                             status)       
+    except KeyError:
+        return jsonify({'message': 'Status field is missing'}), 400
+
+    
