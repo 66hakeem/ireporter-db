dbde48ee79eada2dcf4fc3ee82a250d318306002
66hakeem <66hakeem@gmail.com>
2019-01-24 16:30:38 +0300

    Update redflag models file

--- app/models/redflag_models.py
+++ app/models/redflag_models.py
@@ -26,7 +26,7 @@ class Redflag:
         except psycopg2.Error as err:
             return jsonify({'error': str(err)})
         return jsonify({"status": 201, "data": [{"message": "Created red-flag\
-                                                record"}]}), 201
+        record"}]}), 201
 
     def get_all_redflags(self):
         db_content.dict_cursor.execute("SELECT * FROM incidents WHERE\
@@ -35,27 +35,27 @@ class Redflag:
         return jsonify({'red-flags': data}), 200
 
     def update_redflag_comment(self, id, comment):
-        sql = "UPDATE incidents SET comment='"+comment+"' WHERE incident_type='red-flag' and id='{}'\
+        sql = "UPDATE incidents SET comment='"+comment+"' WHERE incident_type='red-flag' and incident_id='{}'\
                ".format(id)
         db_content.cur.execute(sql)
         return jsonify({"status": 200, "data": [{"message": "Updated red-flag\
                          record's comment"}]}), 200
 
     def update_redflag_location(self, id, location):
-        sql = "UPDATE incidents SET location='"+location+"' WHERE incident_type='red-flag' and id='{}'\
+        sql = "UPDATE incidents SET location='"+location+"' WHERE incident_type='red-flag' and incident_id='{}'\
                 ".format(id)
         db_content.cur.execute(sql)
         return jsonify({"status": 200, "data": [{"message": "Updated red-flag\
                          record's location"}]}), 200
 
     def get_red_flag(self, id):
-        db_content.dict_cursor.execute("SELECT * from incidents WHERE incident_type='red-flag' and id='{}'\
+        db_content.dict_cursor.execute("SELECT * from incidents WHERE incident_type='red-flag' and incident_id='{}'\
                                        ".format(id))
         data = db_content.dict_cursor.fetchall()
         return jsonify({"status": 200, "data": data}), 200
 
     def delete_redflag(self, id):
-        sql = "DELETE from incidents WHERE incident_type='red-flag' and id='{}'\
+        sql = "DELETE from incidents WHERE incident_type='red-flag' and incident_id='{}'\
         ".format(id)
         db_content.cur.execute(sql)
         return jsonify({"status": 200, "message": "Redflag deleted"})
